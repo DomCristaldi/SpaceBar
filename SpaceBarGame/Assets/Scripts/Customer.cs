78cbb10fe723217ec0d89b7e689bc11a48b14869
Bparker10 <parker4@rpi.edu>
2015-09-07 17:27:59 -0400

    things and stuff spawn

--- SpaceBarGame/Assets/Scripts/Customer.cs
+++ SpaceBarGame/Assets/Scripts/Customer.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class Customer : MonoBehaviour {
 
-	public Sprite customer;
-	public Sprite food;
+	public GameObject food;
+	public string foodName;
+	public GameObject bar;
 
-	int happiness;
+	int tip;
+
+	void Start(){
+		//instantiate food here?
+		Vector3 foodSpawn = new Vector3 (transform.position.x, transform.position.y + 150, transform.position.z);
+		Instantiate (food, transform.position, Quaternion.identity);
+		foodName = food.name;
+		bar.GetComponent<Barfood> ().order (food);
+	}
 	
 	// Update is called once per frame
 	void Update () {
--- SpaceBarGame/Assets/Scripts/Spawn.cs
+++ SpaceBarGame/Assets/Scripts/Spawn.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Spawn : MonoBehaviour {
 
-		
+	public List<GameObject> spawnPoints;
+	public List<GameObject> customers;
+	public List<GameObject> food;
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-
+			int c = Random.Range (0,customers.Count);
+			int s = Random.Range(0,spawnPoints.Count);
+			int f = Random.Range (0,food.Count);
+			GameObject tmp = customers[c];
+			tmp.GetComponent<Customer>().food = food[f];
+			Instantiate(tmp,spawnPoints[s].transform.position,Quaternion.identity);
 		}
 	}
 }
