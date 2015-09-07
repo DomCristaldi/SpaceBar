3abefb6d3fff58886a24004a462d854d894e6ccb
Bparker10 <parker4@rpi.edu>
2015-09-07 14:36:36 -0400

    yay placeholders

---
+++ SpaceBarGame/Assets/Scripts/Customer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Customer : MonoBehaviour {
+
+	public Sprite customer;
+	public Sprite food;
+
+	int happiness;
+	
+	// Update is called once per frame
+	void Update () {
+		//time goes by, customer get less happy = lower tip
+	
+	}
+}
--- SpaceBarGame/Assets/Scripts/Spawn.cs
+++ SpaceBarGame/Assets/Scripts/Spawn.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+		
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown (0)) {
+
+		}
 	}
 }
