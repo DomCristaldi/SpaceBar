6d47d3042010359323aea75671e072f1bc6fb95e
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-07 17:26:11 -0400

    Player moves using the PlayerMotor script

---
+++ SpaceBarGame/Assets/Scripts/Motor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Motor : MonoBehaviour {
+
+    protected Transform tf;
+    protected Rigidbody rigBod;
+
+    public float maxMoveSpeed = 1.0f;
+    public float redirectSpeed = 0.25f;
+
+    public Vector3 deliveredDirec;
+    public Vector3 trueDirec;
+
+    //private Vector3 
+
+    void Awake() {
+        tf = GetComponent<Transform>();
+        rigBod = GetComponent<Rigidbody>();   
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        HandleMovement();
+	}
+
+    protected void HandleMovement() {
+        Debug.DrawRay(tf.position, deliveredDirec, Color.red);
+
+        trueDirec = deliveredDirec;
+
+        Debug.DrawRay(tf.position, trueDirec, Color.blue);
+
+        rigBod.velocity = new Vector3(trueDirec.x, 0.0f, trueDirec.z);
+    }
+
+    public void InputPos(Vector3 pos) {
+        InputDirec((pos - tf.position));
+    }
+
+    public void InputDirec(Vector3 direc) {
+        deliveredDirec = Vector3.ClampMagnitude(direc, maxMoveSpeed);
+    }
+}
---
+++ SpaceBarGame/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : Motor {
+
+    public Vector3 mousePoint;
+
+    private Plane playerPlane;
+
+    protected override void Update() {//perform all inputs before we run the motor
+        //base.Update();
+
+        CalculatePlayerPlane();
+
+        CalcMousePoint();
+        InputPos(mousePoint);
+
+        //Debug.DrawLine(tf.position, mousePoint);
+
+        base.Update();
+    }
+
+    private void CalculatePlayerPlane() {
+        //Vector3 planeNorm = Camera.main.transform.rotation * Vector3.one;
+        Vector3 planeNorm = Vector3.up;
+        playerPlane = new Plane(planeNorm, tf.position);
+    }
+
+    private void CalcMousePoint() {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float dist;
+        if (playerPlane.Raycast(camRay, out dist)) {
+            mousePoint = camRay.GetPoint(dist);
+        }
+    }
+
+}
