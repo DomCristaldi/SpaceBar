53a2b0b876826d40689d3c7e44ba5e1a780b785f
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-08 15:19:44 -0400

    .. Parented the Player's collider to the Player model ++ Added a Sorting Order Controller that's gonna be used to make sure things don't overlap

---
+++ SpaceBarGame/Assets/Scripts/SortingOrderController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortingOrderController : MonoBehaviour {
+
+    Renderer renderer;
+
+    void Awake() {
+        renderer = GetComponent<Renderer>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
