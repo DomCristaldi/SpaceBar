0697653ead741becb2ba066da8fb999e609a75d1
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-07 23:19:41 -0400

    ++ Made a global score handler for the Master Controller to streamline score management with the UI

---
+++ SpaceBarGame/Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreHandler : MonoBehaviour {
+
+    public static ScoreHandler singleton;
+
+    public int score = 0;
+
+    void Awake() {
+        if (singleton == null) {
+            singleton = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AddTip(int amount) {
+        score += amount;
+    }
+}
