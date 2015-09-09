75fa94dcbe46dee6a7bc146eaa86c9017230a8c1
Bparker10 <parker4@rpi.edu>
2015-09-08 21:51:42 -0400

    set up triggers to the spawning/food code

---
+++ SpaceBarGame/Assets/Scripts/Food.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Food : MonoBehaviour {
+
+	public GameObject spawnPoint;
+	public Collider trigger;
+	public GameObject food;
+
+	public GameObject pickup(){
+		if (food != null) {
+			GameObject tmp = food;
+			food = null;
+			return tmp;
+		}
+		return food;
+	}
+}
